85ca9423135ad1cab80890652cae31902ef8e78d
Charliehutchison <cmhp2024@mymail.pomona.edu>
2026-02-04 14:26:34 -0800

    File update lab2.java

--- lab2.java
+++ lab2.java
@@ -13,9 +13,7 @@ public class lab2{
     public static int add(int x1, int x2){
         int result1 = x1 + x2;
         return result1;
-    public static int add(int x1, int x2){
-        int result1 = x1 + x2;
-        return result1;
+   
     }
 
     public void main(){
