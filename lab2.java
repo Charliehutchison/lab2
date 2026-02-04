b0744d4f7d79aadfce0d6f3b64d096b49d233d85
isaiahmoligapuletasi <isaiahpuletasi@gmail.com>
2026-02-04 14:18:32 -0800

    w/ AddOne method

--- lab2.java
+++ lab2.java
@@ -3,6 +3,13 @@ public class lab2{
         System.out.println("Hello World");
     }
 
+    public static int[] addOne(int[] listOfNum) {
+        for (int i = 0; i <= listOfNum.length; i++) {
+            listOfNum[i] += 1; 
+        }
+        return listOfNum;
+    }
+
     public void main(){
         helloWorld();
 
